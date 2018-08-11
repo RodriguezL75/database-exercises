8a23a6b1ccaadd99acbe25ccb991078af6221034
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-08-10 22:28:26 -0500

    created table shell

---
+++ albums_migration.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(255) NOT NULL,
+name VARCHAR(255) NOT NULL,
+release_date INT UNSIGNED NOT NULL, -- UNSIGNED A POSITIVE NUM
+sales FLOAT,
+genre VARCHAR(255),
+PRIMARY KEY (id)
+);
+
+DESCRIBE albums; -- to see output
