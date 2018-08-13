2da654a9db937660305f7fb2b3790f9f2b7f38a6
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-08-13 12:49:40 -0500

    select exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,4 +2,13 @@
 
 USE codeup_test_db;
 
+-- -- <action> <output> <direction> <database> <statement><columnName> <comparator> <String>
+-- -- artist, name, release_date, genre, sales
+
+SELECT name from albums WHERE artist like 'Pink%';
+SELECT release_date from albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT genre from albums WHERE name = 'Nevermind';
+SELECT name from albums WHERE release_date BETWEEN 1990 and 1999;
+SELECT name from albums WHERE sales < 20;
+SELECT name from albums WHERE genre like '% rock%';
 
